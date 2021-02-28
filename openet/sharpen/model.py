d31b9c17d95ecb717cc1bbb76c07d7581224331e
Charles Morton <charles.g.morton@gmail.com>
2021-02-28 11:02:11 -0800

    Moved calculation function into Model class

--- openet/sharpen/model.py
+++ openet/sharpen/model.py
@@ -23,210 +23,202 @@ class Model:
 
         Parameters
         ----------
-        input_img : ee.Image
-        sensor : str
+        image : ee.Image
+            "Prepped" Landsat image with standardized bands names
+                (i.e. blue, green, red, nir, swir1, swir2, tir).
+            Thermal band must be named 'tir' and be in units of Kelvin.
+            Must have property 'SATELLITE' with a value of 'LANDSAT_4',
+                'LANDSAT_5', 'LANDSAT_7', or 'LANDSAT_8'.
 
         """
         self.image = image
 
+    # TODO: Decide if this shoudl be a lazy property since it doesn't have inputs
     # @lazy_property
-    def tir(self):
-        """Wrapper to the thermal function"""
-        return thermal(self.image)
+    def thermal(self):
+        """Thermal sharpening algorithm
 
+        Global-RF and local-SLR and a residual redistribution process
 
-# TODO: Move into Model class
-def thermal(image):
-    """Thermal sharpening algorithm
+        Returns
+        -------
+        ee.Image
 
-    Global-RF and local-SLR and a residual redistribution process
+        References
+        ----------
+        https://www.mdpi.com/2072-4292/4/11/3287/htm
 
-    Parameters
-    ----------
-    image : ee.Image
-        "Prepped" Landsat image with standardized bands names
-            (i.e. blue, green, red, nir, swir1, swir2, tir).
-        Thermal band must be named 'tir' and be in units of Kelvin (unscaled).
-        Must have property 'SATELLITE' with a value of 'LANDSAT_4',
-            'LANDSAT_5', 'LANDSAT_7', or 'LANDSAT_8'.
+        """
+        tir_res_dict = ee.Dictionary({
+            'LANDSAT_4': 120, 'LANDSAT_5': 120, 'LANDSAT_7': 60, 'LANDSAT_8': 100})
+        tir_res = ee.Number(tir_res_dict.get(self.image.get('SATELLITE')))
 
-    Returns
-    -------
-    ee.Image
+        # Apply energy conservation step with a slighter large window to reduce blurry effect
+        ec_window_dict = ee.Dictionary({
+            'LANDSAT_4': 120, 'LANDSAT_5': 120, 'LANDSAT_7': 90, 'LANDSAT_8': 120})
+        ec_window = ee.Number(ec_window_dict.get(self.image.get('SATELLITE')))
 
-    References
-    ----------
-    https://www.mdpi.com/2072-4292/4/11/3287/htm
+        kernel_size = 20  # kernel radius for local linear regression,
+        # lower values for more heterogeneous areas
+        cv_threshold = 0.15  # threshold to select homogeneous pixels
+        bands = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']  # predictor bands
 
-    """
-    tir_res_dict = ee.Dictionary({
-        'LANDSAT_4': 120, 'LANDSAT_5': 120, 'LANDSAT_7': 60, 'LANDSAT_8': 100})
-    tir_res = ee.Number(tir_res_dict.get(image.get('SATELLITE')))
+        # TODO: Move to __init__
+        bound = self.image.geometry()
+        crs = self.image.projection().crs()
+        transform = self.getAffineTransform()
 
-    # Apply energy conservation step with a slighter large window to reduce blurry effect
-    ec_window_dict = ee.Dictionary({
-        'LANDSAT_4': 120, 'LANDSAT_5': 120, 'LANDSAT_7': 90, 'LANDSAT_8': 120})
-    ec_window = ee.Number(ec_window_dict.get(image.get('SATELLITE')))
+        tir_transform = transform.set(0, tir_res).set(4, tir_res.multiply(-1))
+        ec_transform = transform.set(0, ec_window).set(4, ec_window.multiply(-1))
 
-    kernel_size = 20  # kernel radius for local linear regression,
-    # lower values for more heterogeneous areas
-    cv_threshold = 0.15  # threshold to select homogeneous pixels
-    bands = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']  # predictor bands
+        # Aggregate the TIR band (result of the cubic convolution interpolation)
+        # Convert to brightness temperature or radiance
+        tir = self.image.select(['tir']) \
+            .reduceResolution(reducer=ee.Reducer.mean(), bestEffort=True) \
+            .reproject(crs=crs, crsTransform=tir_transform) \
+            .pow(4)
 
-    bound = image.geometry()
-    crs = image.projection().crs()
-    transform = getAffineTransform(image)
+        # Aggregating predictor bands for mean value
+        other = self.image.select(bands)
+        other_mean = other \
+            .reduceResolution(reducer=ee.Reducer.mean(), bestEffort=True) \
+            .reproject(crs=crs, crsTransform=tir_transform)
 
-    tir_transform = transform.set(0, tir_res).set(4, tir_res.multiply(-1))
-    ec_transform = transform.set(0, ec_window).set(4, ec_window.multiply(-1))
+        # Aggregating predictor bands for std value
+        other_std = other \
+            .reduceResolution(reducer=ee.Reducer.stdDev(), bestEffort=True) \
+            .reproject(crs=crs, crsTransform=tir_transform)
 
-    # Aggregate the TIR band (result of the cubic convolution interpolation)
-    # Convert to brightness temperature or radiance
-    tir = image.select(['tir']) \
-        .reduceResolution(reducer=ee.Reducer.mean(), bestEffort=True) \
-        .reproject(crs=crs, crsTransform=tir_transform) \
-        .pow(4)
+        # Compute the coefficient of variation of sub-pixel reflectance
+        other_cv = other_std.divide(other_mean).reduce(ee.Reducer.mean()) \
+            .rename(['mean_cv'])
 
-    # Aggregating predictor bands for mean value
-    other = image.select(bands)
-    other_mean = other \
-        .reduceResolution(reducer=ee.Reducer.mean(), bestEffort=True) \
-        .reproject(crs=crs, crsTransform=tir_transform)
+        # Add a bias band (=1) to the image for linear regression reducer
+        image_agg = other_mean \
+            .addBands(other_mean.select([0]).multiply(0).add(1).rename(['bias'])) \
+            .addBands(tir) \
 
-    # Aggregating predictor bands for std value
-    other_std = other \
-        .reduceResolution(reducer=ee.Reducer.stdDev(), bestEffort=True) \
-        .reproject(crs=crs, crsTransform=tir_transform)
+        # Fit moving-window linear regressions at coarse resolution
+        # Y: tir (power 4)
+        # X: SR Bands
+        kernel = ee.Kernel.square(kernel_size)
+        local_fit = image_agg.reduceNeighborhood(
+            ee.Reducer.linearRegression(len(bands) + 1, 1), kernel, None, False)
 
-    # Compute the coefficient of variation of sub-pixel reflectance
-    other_cv = other_std.divide(other_mean).reduce(ee.Reducer.mean()) \
-        .rename(['mean_cv'])
+        # Extract coefficients
+        # Use crsTransform instead of scale to avoid misalignment
+        band_names = bands.copy()
+        band_names.extend(['bias'])
+        coefficients = local_fit.select('coefficients').arrayProject([0]) \
+            .arrayFlatten([band_names]) \
+            .reproject(crs, transform)
 
-    # Add a bias band (=1) to the image for linear regression reducer
-    image_agg = other_mean \
-        .addBands(other_mean.select([0]).multiply(0).add(1).rename(['bias'])) \
-        .addBands(tir) \
+        rmse = local_fit.select('residuals').arrayFlatten([['residuals']]).pow(0.25)
 
-    # Fit moving-window linear regressions at coarse resolution
-    # Y: tir (power 4)
-    # X: SR Bands
-    kernel = ee.Kernel.square(kernel_size)
-    local_fit = image_agg.reduceNeighborhood(
-        ee.Reducer.linearRegression(len(bands) + 1, 1), kernel, None, False)
+        # Apply linear fit at high resolution for sharpened TIR
+        inputs = self.image.select(bands) \
+            .addBands(self.image.select([0]).multiply(0).add(1).rename(['bias']))
+        tir_sp_local = inputs.multiply(coefficients).reduce(ee.Reducer.sum()) \
+            .pow(0.25)
 
-    # Extract coefficients
-    # Use crsTransform instead of scale to avoid misalignment
-    band_names = bands.copy()
-    band_names.extend(['bias'])
-    coefficients = local_fit.select('coefficients').arrayProject([0]) \
-        .arrayFlatten([band_names]) \
-        .reproject(crs, transform)
+        # Fit a scene-wise random forest model
+        # Select homogeneous samples defined by a threshold in c.v.
+        samples = image_agg \
+            .updateMask(other_cv.lt(cv_threshold)) \
+            .sample(region=bound, scale=tir_res, factor=5e-3)
 
-    rmse = local_fit.select('residuals').arrayFlatten([['residuals']]).pow(0.25)
+        # YK - Update: use smileRandomForest
+        rf = ee.Classifier.smileRandomForest(100, 4, 50) \
+            .setOutputMode('REGRESSION') \
+            .train(samples, 'tir', bands)
 
-    # Apply linear fit at high resolution for sharpened TIR
-    inputs = image.select(bands) \
-        .addBands(image.select([0]).multiply(0).add(1).rename(['bias']))
-    tir_sp_local = inputs.multiply(coefficients).reduce(ee.Reducer.sum()) \
-        .pow(0.25)
+        # Apply RF to local resolution (SR bands)
+        tir_sp_global = self.image.classify(rf, 'tir_pred').pow(0.25)
 
-    # Fit a scene-wise random forest model
-    # Select homogeneous samples defined by a threshold in c.v.
-    samples = image_agg \
-        .updateMask(other_cv.lt(cv_threshold)) \
-        .sample(region=bound, scale=tir_res, factor=5e-3)
+        """ Residual analysis """
+        # Aggregate local model results to tir resolution
+        local_agg = tir_sp_local.pow(4) \
+            .reduceResolution(reducer=ee.Reducer.mean(), bestEffort=True) \
+            .reproject(crs=crs, crsTransform=tir_transform) \
+            .pow(0.25)
 
-    # YK - Update: use smileRandomForest
-    rf = ee.Classifier.smileRandomForest(100, 4, 50) \
-        .setOutputMode('REGRESSION') \
-        .train(samples, 'tir', bands)
+        # Aggregate global model results to tir resolution
+        global_agg = tir_sp_global.pow(4) \
+            .reduceResolution(reducer=ee.Reducer.mean(), bestEffort=True) \
+            .reproject(crs=crs, crsTransform=tir_transform) \
+            .pow(0.25)
 
-    # Apply RF to local resolution (SR bands)
-    tir_sp_global = image.classify(rf, 'tir_pred').pow(0.25)
+        # Compute weights based on residuals at coarse resolution
+        # YK - Update: use existing images instead of ee.Image(1)
+        res_local = local_agg.pow(4).subtract(tir).abs()
+        res_global = global_agg.pow(4).subtract(tir).abs()
+        ## res_local_part = ee.Image(1).divide(res_local.pow(2))
+        # res_global_part = ee.Image(1).divide(res_global.pow(2))
+        res_local_part = res_local.multiply(0).add(1).divide(res_local.pow(2))
+        res_global_part = res_global.multiply(0).add(1).divide(res_global.pow(2))
+        weight_local = res_local_part.divide(res_local_part.add(res_global_part))
+        weight_global = res_global_part.divide(res_local_part.add(res_global_part))
 
-    """ Residual analysis """
-    # Aggregate local model results to tir resolution
-    local_agg = tir_sp_local.pow(4) \
-        .reduceResolution(reducer=ee.Reducer.mean(), bestEffort=True) \
-        .reproject(crs=crs, crsTransform=tir_transform) \
-        .pow(0.25)
+        # No residual equals to 0
+        valid_mask = res_local.gt(0).multiply(res_global.gt(0))
 
-    # Aggregate global model results to tir resolution
-    global_agg = tir_sp_global.pow(4) \
-        .reduceResolution(reducer=ee.Reducer.mean(), bestEffort=True) \
-        .reproject(crs=crs, crsTransform=tir_transform) \
-        .pow(0.25)
+        # Compute weighted average of local and global model results
+        # Weighted sum
+        tir_sp_final = tir_sp_local.pow(4).multiply(weight_local) \
+            .add(tir_sp_global.pow(4).multiply(weight_global)) \
+            .multiply(valid_mask).pow(0.25)
+        # Local residual is 0
+        tir_sp_final = tir_sp_final.add(tir_sp_local.multiply(res_local.eq(0)))
+        # Global residual is 0
+        tir_sp_final = tir_sp_final.add(tir_sp_global.multiply(res_global.eq(0)))
 
-    # Compute weights based on residuals at coarse resolution
-    # YK - Update: use existing images instead of ee.Image(1)
-    res_local = local_agg.pow(4).subtract(tir).abs()
-    res_global = global_agg.pow(4).subtract(tir).abs()
-    ## res_local_part = ee.Image(1).divide(res_local.pow(2))
-    # res_global_part = ee.Image(1).divide(res_global.pow(2))
-    res_local_part = res_local.multiply(0).add(1).divide(res_local.pow(2))
-    res_global_part = res_global.multiply(0).add(1).divide(res_global.pow(2))
-    weight_local = res_local_part.divide(res_local_part.add(res_global_part))
-    weight_global = res_global_part.divide(res_local_part.add(res_global_part))
+        """ YK - UPDATE: Apply Energy Conservation to combined sharpened image """
+        # Aggregated sharpened image
+        tir_sp_agg = tir_sp_final.pow(4) \
+            .reduceResolution(ee.Reducer.mean()) \
+            .reproject(crs=crs, crsTransform=ec_transform).pow(0.25)
+        # Aggregate original TIR image
+        tir_org_agg = self.image.select(['tir']).pow(4) \
+            .reduceResolution(ee.Reducer.mean()) \
+            .reproject(crs=crs, crsTransform=ec_transform).pow(0.25)
 
-    # No residual equals to 0
-    valid_mask = res_local.gt(0).multiply(res_global.gt(0))
+        # Interpolate residuals
+        res_sp = tir_sp_agg.subtract(tir_org_agg)
+        res_conv = res_sp.resample('bilinear').reproject(crs,transform)
 
-    # Compute weighted average of local and global model results
-    # Weighted sum
-    tir_sp_final = tir_sp_local.pow(4).multiply(weight_local) \
-        .add(tir_sp_global.pow(4).multiply(weight_global)) \
-        .multiply(valid_mask).pow(0.25)
-    # Local residual is 0
-    tir_sp_final = tir_sp_final.add(tir_sp_local.multiply(res_local.eq(0)))
-    # Global residual is 0
-    tir_sp_final = tir_sp_final.add(tir_sp_global.multiply(res_global.eq(0)))
+        # Add interpolated residual back to sharpened image
+        tir_sp_ec = tir_sp_final.subtract(res_conv)
 
-    """ YK - UPDATE: Apply Energy Conservation to combined sharpened image """
-    # Aggregated sharpened image
-    tir_sp_agg = tir_sp_final.pow(4) \
-        .reduceResolution(ee.Reducer.mean()) \
-        .reproject(crs=crs, crsTransform=ec_transform).pow(0.25)
-    # Aggregate original TIR image
-    tir_org_agg = image.select(['tir']).pow(4) \
-        .reduceResolution(ee.Reducer.mean()) \
-        .reproject(crs=crs, crsTransform=ec_transform).pow(0.25)
+        # Prepare output
+        out = tir_sp_ec.rename(['tir_sharpened'])
 
-    # Interpolate residuals
-    res_sp = tir_sp_agg.subtract(tir_org_agg)
-    res_conv = res_sp.resample('bilinear').reproject(crs,transform)
+        # # TODO: Add flag/parameter to control if all bands are exported
+        # out = out.addBands(tir_sp_final.rename(['tir_sharpened_non_ec'])) \
+        #     .addBands(image.select('tir').rename(['tir_original'])) \
+        #     .addBands(tir_sp_local.rename(['tir_sp_local'])) \
+        #     .addBands(tir_sp_global.rename(['tir_sp_global'])) \
+        #     .addBands(weight_local.rename(['local_weights'])) \
+        #     .addBands(rmse.rename(['slr_rmse']))
 
-    # Add interpolated residual back to sharpened image
-    tir_sp_ec = tir_sp_final.subtract(res_conv)
+        # CGM - Commenting out adding the other bands for now
+        # out = out.addBands(image.select('tir').rename(['tir_original'])) \
+        #     .addBands(tir.pow(0.25).rename(['tir_agg'])) \
+        #     .addBands(tir_sp_local.rename(['tir_sp_local'])) \
+        #     .addBands(tir_sp_global.rename(['tir_sp_global'])) \
+        #     .addBands(local_agg.rename(['tir_local_agg'])) \
+        #     .addBands(global_agg.rename(['tir_global_agg'])) \
+        #     .addBands(weight_local.rename(['local_weights'])) \
+        #     .addBands(rmse.rename(['slr_rmse']))
 
-    # Prepare output
-    out = tir_sp_ec.rename(['tir_sharpened'])
+        # CGM - copyProperties sometimes drops the type
+        out = ee.Image(out.copyProperties(self.image)) \
+            .set({'system:time_start': self.image.get('system:time_start'),
+                  'energy_conservation': 'True',
+                  })
 
-    # # TODO: Add flag/parameter to control if all bands are exported
-    # out = out.addBands(tir_sp_final.rename(['tir_sharpened_non_ec'])) \
-    #     .addBands(image.select('tir').rename(['tir_original'])) \
-    #     .addBands(tir_sp_local.rename(['tir_sp_local'])) \
-    #     .addBands(tir_sp_global.rename(['tir_sp_global'])) \
-    #     .addBands(weight_local.rename(['local_weights'])) \
-    #     .addBands(rmse.rename(['slr_rmse']))
+        return out
 
-    # CM - Commenting out adding the other bands for now
-    # out = out.addBands(image.select('tir').rename(['tir_original'])) \
-    #     .addBands(tir.pow(0.25).rename(['tir_agg'])) \
-    #     .addBands(tir_sp_local.rename(['tir_sp_local'])) \
-    #     .addBands(tir_sp_global.rename(['tir_sp_global'])) \
-    #     .addBands(local_agg.rename(['tir_local_agg'])) \
-    #     .addBands(global_agg.rename(['tir_global_agg'])) \
-    #     .addBands(weight_local.rename(['local_weights'])) \
-    #     .addBands(rmse.rename(['slr_rmse']))
-
-    out = ee.Image(out.copyProperties(image)) \
-        .set({'system:time_start': image.get('system:time_start'),
-              'energy_conservation': 'True'})
-
-    return out
-
-
-def getAffineTransform(image):
-    projection = image.projection()
-    json = ee.Dictionary(ee.Algorithms.Describe(projection))
-    return ee.List(json.get('transform'))
+    def getAffineTransform(self):
+        projection = self.image.projection()
+        json = ee.Dictionary(ee.Algorithms.Describe(projection))
+        return ee.List(json.get('transform'))
--- openet/sharpen/tests/test_model.py
+++ openet/sharpen/tests/test_model.py
@@ -18,40 +18,15 @@ def test_ee_init():
     assert ee.Number(1).getInfo() == 1
 
 
-def test_sharpen_model_thermal_band_name():
-    input_img = openet.sharpen.Landsat(TEST_IMAGE_ID).image
-    output = openet.sharpen.model.thermal(input_img).bandNames().getInfo()
-    assert output == ['tir_sharpened']
-
-
-@pytest.mark.parametrize(
-    "image_id, xy, expected",
-    [
-        ['LANDSAT/LC08/C01/T1_SR/LC08_042034_20180705',
-         [-120.10237, 36.946608], 304.13262575660264],
-        ['LANDSAT/LC08/C01/T1_TOA/LC08_042034_20180705',
-         [-120.10237, 36.946608], 303.75037059315696],
-    ]
-)
-def test_sharpen_model_thermal_point_values(image_id, xy, expected, tol=0.01):
-    input_img = openet.sharpen.Landsat(image_id).image
-    output_img = openet.sharpen.model.thermal(input_img)
-    output = utils.point_image_value(
-        output_img.select(['tir_sharpened']), xy, scale=30)['tir_sharpened']
-    assert abs(output - expected) < tol
-
-
-# CGM - Until functions are moved into Model class, test Model after functions
-
 def test_Model_init():
     input_img = ee.Image.constant(DEFAULT_VALUES).rename(DEFAULT_BANDS)
     image_obj = openet.sharpen.Model(image=input_img)
     assert set(image_obj.image.bandNames().getInfo()) == set(DEFAULT_BANDS)
 
 
-def test_Model_tir_band_name():
+def test_Model_thermal_band_name():
     input_img = openet.sharpen.Landsat(TEST_IMAGE_ID).image
-    output = openet.sharpen.Model(input_img).tir().bandNames().getInfo()
+    output = openet.sharpen.Model(input_img).thermal().bandNames().getInfo()
     assert output == ['tir_sharpened']
 
 
@@ -64,8 +39,8 @@ def test_Model_tir_band_name():
          [-120.10237, 36.946608], 303.75037059315696],
     ]
 )
-def test_Model_tir_point_values(image_id, xy, expected, tol=0.01):
+def test_Model_thermal_point_values(image_id, xy, expected, tol=0.01):
     input_img = openet.sharpen.Landsat(image_id).image
-    output_img = openet.sharpen.Model(input_img).tir()
+    output_img = openet.sharpen.Model(input_img).thermal()
     output = utils.point_image_value(output_img, xy, scale=30)
     assert abs(output['tir_sharpened'] - expected) < tol
