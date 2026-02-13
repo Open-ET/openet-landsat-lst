e83b012ab2572cbdc2d74d2c0a2d38b16b02a2d0
Charles Morton <cgmorton@users.noreply.github.com>
2026-02-13 14:16:08 -0800

    Switching github actions to use workload identity federation for authentication

--- openet/lst/__init__.py
+++ openet/lst/__init__.py
@@ -1,3 +1,8 @@
 from .model import Model
 from .landsat import Landsat
 from .landsat import Landsat_C02_L2
+
+# from importlib import metadata
+#
+# __version__ = metadata.version(__package__.replace('.', '-') or __name__.replace('.', '-'))
+# # __version__ = metadata.version('openet-landsat-lst')
--- openet/lst/tests/conftest.py
+++ openet/lst/tests/conftest.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 import ee
+import google.oauth2.credentials
 import pytest
 
 
@@ -12,8 +13,11 @@ def test_init():
     logging.getLogger('googleapiclient').setLevel(logging.ERROR)
     logging.debug('Test Setup')
 
-    # For GitHub Actions authenticate using private key environment variable
-    if 'EE_PRIVATE_KEY_B64' in os.environ:
+    # For GitHub Actions authenticate using workload identify federation
+    if "ACTION_EE_TOKEN" in os.environ:
+        creds = google.oauth2.credentials.Credentials(os.getenv('ACTION_EE_TOKEN'))
+        ee.Initialize(creds, project=os.getenv('PROJECT_ID'))
+    elif 'EE_PRIVATE_KEY_B64' in os.environ:
         print('Writing privatekey.json from environmental variable ...')
         content = base64.b64decode(os.environ['EE_PRIVATE_KEY_B64']).decode('ascii')
         EE_KEY_FILE = 'privatekey.json'
